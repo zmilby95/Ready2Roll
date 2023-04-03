3e4bee61cdc2cf0665e3c6869727b7398a1a3d25
Zach Milby <zachary.milby@gmail.com>
2023-04-03 15:26:20 -0400

    updated the DiceRoll class

--- Ready2Roll/Ready2Roll/DiceRoll.cs
+++ Ready2Roll/Ready2Roll/DiceRoll.cs
@@ -8,29 +8,5 @@ namespace Ready2Roll
 {
     public class DiceRoll
     {
-        private Random random;
-        private int sidesCount;
-
-        public DiceRoll()
-        {
-            sidesCount = 6;
-            random = new Random();
-        }
-
-        public DiceRoll(int sidesCount)
-        {
-            this.sidesCount = sidesCount;
-            random = new Random();
-        }
-
-        public int GetSidesCount()
-        {
-            return sidesCount;
-        }
-
-        public int Roll()
-        {
-            return random.Next(1, sidesCount + 1);
-        }
-    }
-}    
+        
+    }    
