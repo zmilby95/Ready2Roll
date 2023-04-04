403f9594df5fe48e982ca2c5ab25e70a2268aa19
Zach Milby <zachary.milby@gmail.com>
2023-04-03 21:48:19 -0400

    reworked classes and loops

--- Ready2Roll/Ready2Roll/Program.cs
+++ Ready2Roll/Ready2Roll/Program.cs
@@ -1,43 +1,66 @@
 ﻿using System;
 using Ready2Roll;
 
-var calculator = new Calculator();
+
+
+
 var input = Console.ReadLine();
+
 
 //introduction 
 
 Console.WriteLine("     Hello, welcome to Ready to Roll");
 
+
 //loop
-static void Main(string[] args);
+    static void Main(string[] args)
 {
-    var repeat = true;
-    while (repeat) ;
+    List<Dice> dices = new List<Dice>();
 
-
-    Console.WriteLine("How many dice would you like to roll?");
-    int dice = int.Parse(Console.ReadLine());
-
-    Console.WriteLine("How many sides do the dice have? ");
-    int sides = int.Parse(Console.ReadLine());
-
-
-    //Dice roll + adding 
-    
-
-    // roll again or exit 
-
-    Console.WriteLine("Would you like to roll again? Press 1 for yes 2 to quit.");
-    Console.ReadLine();
-
-    switch (input)
+    int dice = 0;
+    do 
     {
-        case "1":
+        if (dice == 0)
+        {
+            Console.WriteLine("How many dice would you like to roll? press 0 to quit");
+            dice = int.Parse(Console.ReadLine());
+        }
 
-
-        case "2":
+        if (dice == 0)
+        {
             Console.WriteLine("Thank you for rolling!");
             break;
-    }
+        }
 
+        for (int i = 0; i < dice; i++)
+        { 
+        Console.WriteLine("How many sides do the dice have? press 0 to quit");
+        int sides = int.Parse(Console.ReadLine());
+            if (sides <0)
+            {
+                Console.WriteLine("sides can not be negative. Please enter a positive integer or 0 to quit");
+                i--;
+                continue;
+            }
+            if (sides == 0)
+            {
+                Console.WriteLine("Thank you for rolling!");
+                return;
+            }
+            Dice die = new Dice();
+            die.sides = sides;
+            dices.Add(die);
+        }
+    } while (dice > 0);
+
+    //Dice roll 
+
+    int roll = 0;
+
+        // store the total for the day
+
+        dice--;
+
+
+    
 }
